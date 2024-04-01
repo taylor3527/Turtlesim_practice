ff703d131adaae92babea6e41f102f37a34b3270
taylor3527 <164743786+taylor3527@users.noreply.github.com>
2024-04-01 19:10:39 +0900

    Solopy_ws/src
    Source file

---
+++ src/Solopy_position/Solopy_position/Solopy_node.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+from Solopy_class import SoloPMSM
+
+def main()
+    
+    MySolo = SoloPMSM()
+
+    mySolo = MySolo.Instantiate()
+
+if __name__ == '__main__':
+    
+    main()
---
+++ src/Solopy_position/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'Solopy_position'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='taylor3527',
+    maintainer_email='taylor3527@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'Solopy_node = Solopy_position.Solopy_node:main'
+        ],
+    },
+)
